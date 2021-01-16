3506706377b7b4c99c5dad6a4a3be186f0ade6f5
Vadim <vadim.prog.2000@gmail.com>
2021-01-16 23:57:03 +0300

    add Router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
+import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import CardList from "./components/CardList/CardList"
 
 const App = () => {
 	return (
-		<div className="container">
-			<CardList />
-		</div>
+		<BrowserRouter>
+			<div className="container">
+				<Switch>
+					<Route path="/cards" exact component={CardList}/>
+					<Route path="/card/:id" exact component={CardList}/>
+					<Redirect to="/cards" />
+				</Switch>
+			</div>
+		</BrowserRouter>
 	);
 }
 
