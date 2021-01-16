8d275effffd9cf4b1ca462bcfbfdd89e157cdba8
Vadim <vadim.prog.2000@gmail.com>
2021-01-16 23:52:31 +0300

    complete structure & style for CardItem

--- src/components/CardList/CardItem/CardItem.js
+++ src/components/CardList/CardItem/CardItem.js
@@ -3,7 +3,10 @@ import './CardItem.css'
 const CardItem = ({card}) => {
 	return (
 		<li className="card-item">
-			{card.title}
+			<span className="card-item__title">{card.title}</span>
+			<img className="card-item__img" src="https://cdn.iconscout.com/icon/free/png-256/gift-1020-542634.png" alt="product"/>
+			<span className="card-item__price">{card.price} руб.</span>
+			<button className="card-item__button">{card.selected ? '-' : '+'}</button>
 		</li>
 	)
 }
