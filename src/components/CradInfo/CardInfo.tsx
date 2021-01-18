6670f0b77c185464dd4de09ba805c38d352810fd
Vadim <vadim.prog.2000@gmail.com>
2021-01-18 22:21:01 +0300

    rewrite Toasts JS -> TS

--- src/components/CradInfo/CardInfo.tsx
+++ src/components/CradInfo/CardInfo.tsx
@@ -1,5 +1,6 @@
 import { computed } from 'mobx'
 import { observer } from 'mobx-react'
+import { FunctionComponent } from 'react'
 import { useRouteMatch } from 'react-router-dom'
 import store from '../../store'
 import './CardInfo.css'
@@ -8,27 +9,26 @@ interface MatchParams {
 	id: string;
 }
 
-const CardInfo = () => {
+const CardInfo: FunctionComponent = () => {
 	const match = useRouteMatch<MatchParams>()
 	const {toggleSelectedCards, cardById} = store
 	const card = computed(() => cardById(match.params.id)).get()
+
+	if (!card) {
+		return  <p>Карточки с таким товаром не существует! :)</p>
+	}
+
 	return (
-		<>
-			{
-				card
-				? <div className="card-info">
-					<img className="card-info__img" src="https://cdn.iconscout.com/icon/free/png-256/gift-1020-542634.png" alt="product"/>
-					<span className="card-info__title">{card.title}</span>
-					<p className="card-info__description">{card.description}</p>
-					<span className="card-info__price">{card.price} руб.</span>
-					<button
-						className="card-info__button"
-						onClick={() => toggleSelectedCards(card.id)}
-					>{card.selected ? '-' : '+'}</button>
-				</div>
-				: <p>Карточки с таким товаром не существует! :)</p>
-			}
-		</>
+		<div className="card-info">
+			<img className="card-info__img" src="https://cdn.iconscout.com/icon/free/png-256/gift-1020-542634.png" alt="product"/>
+			<span className="card-info__title">{card.title}</span>
+			<p className="card-info__description">{card.description}</p>
+			<span className="card-info__price">{card.price} руб.</span>
+			<button
+				className="card-info__button"
+				onClick={() => toggleSelectedCards(card.id)}
+			>{card.selected ? '-' : '+'}</button>
+		</div>
 	)
 }
 
--- src/components/Toasts/Toasts.tsx
+++ src/components/Toasts/Toasts.tsx
@@ -1,33 +1,39 @@
 import { observer } from 'mobx-react'
-import { useCallback, useEffect } from 'react'
+import { FunctionComponent, useCallback, useEffect } from 'react'
 import store from '../../store'
 import './Toasts.css'
 
-const Toasts = () => {
+const Toasts : FunctionComponent = () => {
 	const {toasts, deleteToast} = store
 
 	const dragToast = useCallback((id) => {
+
 		const element = document.getElementById(id)
+
+		if (element === null) {
+			return
+		}
+
 		let shift = 0, position = 0
-		element.onmousedown = dragMouseDown;
+		element!.onmousedown = dragMouseDown;
 		
-		function dragMouseDown(e) {
+		function dragMouseDown(e: any) {
 			position = e.clientX;
 			document.onmouseup = closeDragElement;
 			document.onmousemove = elementDrag;
 		}
 		
-		function elementDrag(e) {
+		function elementDrag(e: any) {
 			shift = position - e.clientX;
 			position = e.clientX;
-			element.style.left = (element.offsetLeft - shift) + "px";
+			element!.style.left = (element!.offsetLeft - shift) + "px";
 		}
 		
 		function closeDragElement() {
-			if (element.offsetLeft - shift > 150 || element.offsetLeft - shift < -150) {
+			if (element!.offsetLeft - shift > 150 || element!.offsetLeft - shift < -150) {
 				deleteToast(id)
 			} else {
-				element.style.left = "0px";
+				element!.style.left = "0px";
 			}
 			document.onmouseup = null;
 			document.onmousemove = null;
